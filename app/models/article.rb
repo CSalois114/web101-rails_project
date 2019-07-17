34427ec5f18b232c9510d90a473cbb38e9bead0d
Chris <csalois@gmail.com>
2019-07-17 19:20:51 -0400

    Fix 'FOREIGN KEY' error with deletions

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
     has_many :comments
-    has_many :taggings
+    has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
+
+    def tag_list
+        tags.join(", ")
+    end
 
     def tag_list=(tags_string)
         tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
